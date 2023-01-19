093f378eb7a70025bbc95af654bcd7720eab4714
Ken Kirui <31723768+kenkirui@users.noreply.github.com>
2023-01-19 11:48:17 +0300

    separation of inbound and outbound stats

--- ICL.DWH.Backend.Core/Migrations/20230119072921_update_statistics_view.cs
+++ ICL.DWH.Backend.Core/Migrations/20230119072921_update_statistics_view.cs
@@ -4,23 +4,25 @@
 
 namespace ICL.DWH.Backend.Core.Migrations
 {
-    public partial class create_statistics_view : Migration
+    public partial class update_statistics_view : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"DROP VIEW IF EXISTS public.statistics;
                 CREATE OR REPLACE VIEW public.statistics as
-                select a.createdate, sum(pending) as pending, sum(delivered) as delivered, sum(failed) as failed 
+                select a.createdate, processtype, sum(pending) as pending, sum(delivered) as delivered, sum(failed) as failed 
                 from
                 (
                 SELECT ""CreateDate""::TIMESTAMP::DATE as createdate
                 , case when ""DeliveryStatus"" = 0 then 1 else 0 end as pending
                 , case when ""DeliveryStatus"" = 1 then 1 else 0 end as delivered
                 , case when ""DeliveryStatus"" = 2 then 1 else 0 end as failed
+                , ""ProcessType"" as processtype
                 FROM public.ft_purchase_orders
                 ) a
-                group by a.createdate;");
+                group by a.createdate, processType;");
         }
+
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"DROP VIEW public.statistics;");
